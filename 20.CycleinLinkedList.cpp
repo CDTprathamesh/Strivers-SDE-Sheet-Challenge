0c5f84ff9db2072c61897bd4785c98ebc4d90b40
CDTprathamesh <130147310+CDTprathamesh@users.noreply.github.com>
2023-06-14 16:18:39 +0530

    Update 20.CycleinLinkedList.cpp

--- 20.CycleinLinkedList.cpp
+++ 20.CycleinLinkedList.cpp
@@ -27,6 +27,8 @@
 
 *****************************************************************/
 #include <unordered_map>
+#include<limits.h>
+
 bool detectCycle(Node *head)
 {
 	//	Write your code here
@@ -39,5 +41,48 @@ bool detectCycle(Node *head)
         mp[temp]=true;
         temp=temp->next;
     }
+    return false;
+	
+	
+	
+	
+	
+    Node* fast=head;
+    Node* slow=head;
+    int n=1;
+    while(slow!=NULL && fast != NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        if(slow==fast) return true;
+    }
+    return false;
+
+	
+	
+	
+	
+	
+	
+    while(head!=nullptr)
+
+    {
+
+        if(head->data==INT_MIN)
+
+        {
+
+            return true;
+
+        }
+
+        head->data = INT_MIN;
+
+        head = head->next;
+
+    }
+
     return false;
 }
